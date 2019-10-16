4f6c7d84b8c0776b357747e54a182e57ae872acd
Sohail-ur-rahman <ssohailmails@gmail.com>
2019-10-16 12:53:11 +0530

    Initial Commit

---
+++ AboutPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutPlaces
+//
+//  Created by sohail ur rahman on 16/10/19.
+//  Copyright © 2019 sohail ur rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
